fcbe6add52a8ea5a212eb32e1f2fd46a6965d384
Oleksandr Sabov <alexsabov@gmail.com>
2015-06-20 21:04:29 +0200

    Add searchbar

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ var mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
 });
+
+var mySearchbar = myApp.searchbar('.searchbar', {
+    searchList: '.list-block-search',
+    searchIn: '.item-title'
+});
+
+$$(".item-inner").on("click", function () {
+  console.log(this);
+  $$(".searchbar-input input").val($$(this).find(".item-title").text());
+  $$(".list-block-search").hide();
+});
